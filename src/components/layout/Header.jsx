e9958a9f1aed81d167be4fee37a2231bca909ddf
Ingebrigt Furnes Bøe <ingebrigt03@gmail.com>
2025-03-06 20:53:57 +0100

    fxing bugs

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <header className="bg-white">
             <div className="flex flex-row justify-between px-6 items-center h-[140px] max-w-[1400px] mx-auto">
             <div className="text-black">
-                <Link to="/"><img src="src/assets/logo-ecom.png" className="w-[160px]" alt="logo" /></Link>
+                <Link to="/"><img src="./src/assets/logo-ecom.png" className="w-[160px]" alt="logo" /></Link>
             </div>
             <nav>
             <ul className="flex flex-row gap-6 text-black">
