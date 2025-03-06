a6168cf6e2bbe894b86fa1603bc0c43b95e36a27
Ingebrigt Furnes Bøe <ingebrigt03@gmail.com>
2025-03-06 20:59:41 +0100

    fxing bugs

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router";
 import Cart from "./Cart";
+import logo from "/src//assets/logo-ecom.png"
 
 
 const Header = () => {
@@ -7,7 +8,8 @@ const Header = () => {
         <header className="bg-white">
             <div className="flex flex-row justify-between px-6 items-center h-[140px] max-w-[1400px] mx-auto">
             <div className="text-black">
-                <Link to="/"><img src="/src/assets/logo-ecom.png" className="w-[160px]" alt="logo" /></Link>
+                <Link to="/">
+                <img src={logo} alt="Logo" className="w-42 h-auto" /></Link>
             </div>
             <nav>
             <ul className="flex flex-row gap-6 text-black">
